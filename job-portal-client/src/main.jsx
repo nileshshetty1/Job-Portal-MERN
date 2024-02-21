b82bbfe01ce1389c2297914b194446b99795b7f6
Plix1 <nileshshetty45@gmail.com>
2024-02-21 21:01:24 +0530

    updated nodemon to node and rewrote the router path

--- job-portal-client/src/main.jsx
+++ job-portal-client/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import router from "./Router/router.jsx";
+import router from "./Router/Router.jsx";
 import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
