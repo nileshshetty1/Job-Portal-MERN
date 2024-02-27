c74b726814c5e948a07535afe134d9eb21c7b3fc
Plix1 <nileshshetty45@gmail.com>
2024-02-27 15:52:29 +0530

    fixed the bug where driver was running after every input

--- job-portal-client/src/components/Login.jsx
+++ job-portal-client/src/components/Login.jsx
@@ -1,31 +1,30 @@
 import React from "react";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+import { getAuth } from "firebase/auth";
 import app from "../firebase/firebase.config";
+
 const Login = () => {
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
-  auth.languageCode = "it";
   const handleLogin = () => {
     signInWithPopup(auth, googleProvider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
         const user = result.user;
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
         console.log(user);
       })
       .catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
+        console.log("error code", errorCode);
         const errorMessage = error.message;
+        console.log("errorMessage", errorMessage);
         // The email of the user's account used.
         const email = error.customData.email;
+        console.log("error email", email);
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
+        console.log("credential", credential);
       });
   };
 
--- job-portal-client/src/firebase/firebase.config.js
+++ job-portal-client/src/firebase/firebase.config.js
@@ -1,8 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyARXVDOrEpg2dexnGlbzx-emZt8TDAd0c4",
@@ -15,4 +11,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
